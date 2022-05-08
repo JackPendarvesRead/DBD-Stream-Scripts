501869b5dde52b502f35a564cbcdff578d49ad7e
Jack Read <jackpendarvesread@gmail.com>
2022-05-08 19:35:25 +0100

    Initial

---
+++ RandomKiller/killers.js
@@ -0,0 +1,110 @@
+const killers = [
+  {
+    name: "Trapper",
+    image: "./Images/KillerPortraits/Trapper.png",
+  },
+  {
+    name: "Wraith",
+    image: "./Images/KillerPortraits/Wraith.png",
+  },
+  {
+    name: "HillBilly",
+    image: "./Images/KillerPortraits/Billy.png",
+  },
+  {
+    name: "Nurse",
+    image: "./Images/KillerPortraits/Nurse.png",
+  },
+  {
+    name: "Shape",
+    image: "./Images/KillerPortraits/Shape.png",
+  },
+  {
+    name: "Hag",
+    image: "./Images/KillerPortraits/Hag.png",
+  },
+  {
+    name: "Doctor",
+    image: "./Images/KillerPortraits/Doctor.png",
+  },
+  {
+    name: "Huntress",
+    image: "./Images/KillerPortraits/Huntress.png",
+  },
+  {
+    name: "Cannibal",
+    image: "./Images/KillerPortraits/Cannibal.png",
+  },
+  {
+    name: "Nightmare",
+    image: "./Images/KillerPortraits/Nightmare.png",
+  },
+  {
+    name: "Pig",
+    image: "./Images/KillerPortraits/Pig.png",
+  },
+  {
+    name: "Clown",
+    image: "./Images/KillerPortraits/Clown.png",
+  },
+  {
+    name: "Spirit",
+    image: "./Images/KillerPortraits/Spirit.png",
+  },
+  {
+    name: "Legion",
+    image: "./Images/KillerPortraits/Legion.png",
+  },
+  {
+    name: "Plague",
+    image: "./Images/KillerPortraits/Plague.png",
+  },
+  {
+    name: "Ghostface",
+    image: "./Images/KillerPortraits/Ghostface.png",
+  },
+  {
+    name: "Demogorgan",
+    image: "./Images/KillerPortraits/Demogorgan.png",
+  },
+  {
+    name: "Oni",
+    image: "./Images/KillerPortraits/Oni.png",
+  },
+  {
+    name: "Deathslinger",
+    image: "./Images/KillerPortraits/Deathslinger.png",
+  },
+  {
+    name: "Executioner",
+    image: "./Images/KillerPortraits/Executioner.png",
+  },
+  {
+    name: "Blight",
+    image: "./Images/KillerPortraits/Blight.png",
+  },
+  {
+    name: "Twins",
+    image: "./Images/KillerPortraits/Twins.png",
+  },
+  {
+    name: "Trickster",
+    image: "./Images/KillerPortraits/Trickster.png",
+  },
+  {
+    name: "Nemesis",
+    image: "./Images/KillerPortraits/Nemesis.png",
+  },
+  {
+    name: "Cenobite",
+    image: "./Images/KillerPortraits/Cenobite.png",
+  },
+  {
+    name: "Artist",
+    image: "./Images/KillerPortraits/Artist.png",
+  },
+  {
+    name: "Onryo",
+    image: "./Images/KillerPortraits/Onryo.png",
+  },
+];
